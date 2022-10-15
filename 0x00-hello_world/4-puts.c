638b0d8b0ebcfe045171349d787fc27705d5a533
ODERROW <Brollineodero@gmail.com>
2022-10-14 17:59:46 -0700

    to commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+ * Return: Always 0				 */
+int main(void)					{							puts("\"Programming is like building a multilingual puzzle");					return (0);
 }	
